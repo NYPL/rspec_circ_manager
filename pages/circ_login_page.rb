bac40699e892f915e1eca7fea297ea6b2813fe30
jcsmit17 <jonathansmith7656@gmail.com>
2021-05-14 11:49:22 -0400

    created first cut of automation

---
+++ pages/circ_login_page.rb
@@ -0,0 +1,40 @@
+class CircLoginPage
+    include Selenium
+
+    def initialize(browser)
+        @browser                = browser
+        @email_text_field       = @browser.text_field(name: "email")
+        @password_text_field    = @browser.text_field(name: "password")
+        @submit_button          = @browser.element(type: "submit")
+
+        @login_url              = "https://dev-circulation.librarysimplified.org/admin/web"
+        @admin_portal_url       = "https://dev-circulation.librarysimplified.org/admin/web/config/libraries"
+    end
+
+    def goto_url
+        @browser.goto @login_url
+    end
+
+    def login_as(email, password)
+        @email_text_field.wait_until(&:present?).set(email)
+        @password_text_field.wait_until(&:present?).set(password)
+        @submit_button.wait_until(&:present?).click
+    end
+
+    def navigate_to_admin_page_as(email, password)
+        @email_text_field.wait_until(&:present?).set(email)
+        @password_text_field.wait_until(&:present?).set(password)
+        @submit_button.wait_until(&:present?).click
+        @submit_button.wait_while(&:present?)
+        sleep(5)
+        @browser.goto @admin_portal_url
+        sleep(5)
+        @email_text_field.wait_until(&:present?).set(email)
+        @password_text_field.wait_until(&:present?).set(password)
+        @submit_button.wait_until(&:present?).click
+        @submit_button.wait_while(&:present?)
+        sleep(5)
+        @browser.goto @admin_portal_url
+        sleep(5)
+    end
+end
---
+++ spec/admin_form/007_logging_tab_spec.rb
@@ -0,0 +1,45 @@
+require 'watir'
+
+require_relative "../../pages/circ_login_page.rb"
+require_relative "../../pages/circ_admin_page.rb"
+
+RSpec.describe "007: Logging Tab" do
+    before(:all) do
+    end
+
+    after(:all) do
+    end
+
+    before(:each) do
+        client = Selenium::WebDriver::Remote::Http::Default.new
+        client.open_timeout = 180
+        client.read_timeout = 180
+
+        @browser = Watir::Browser.new :chrome, :http_client => client
+
+        @login_page = CircLoginPage.new(@browser)
+        @admin_page = CircAdminPage.new(@browser)
+        
+        # @admin_form = CircAdminAdminsForm.new(@browser)
+    end
+
+    after(:each) do
+        # @admin_page.delete_by_value(browser_instance, tab, entry_value, loading_message)
+        @browser.close
+    end
+
+    xit "Returns success message with valid form fill" do
+        ENV['CIRC_USERNAME'] = "consultjsmith@nypl.org"
+        ENV['CIRC_PASSWORD'] = "turtlepower"
+
+        @login_page.goto_url
+        @login_page.login_as(ENV['CIRC_USERNAME'], ENV['CIRC_PASSWORD'])
+        @admin_page.goto_url
+        
+        # goto tab
+        
+        # fill form
+
+        expect(@admin_page.success_message.present?).to eql true
+    end
+end
