4ff6385ec3611f7b81b2a294b37013351be8f864
Jonathan Smith <jonathansmith7656@gmail.com>
2021-05-20 20:25:51 -0400

    Remove comment with passwords
    Security does not work if you have passwords in the open :)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,6 @@ require "dotenv/load"
 require "rspec/retry"
 require "allure-rspec"
 
-# ENV['CIRC_USERNAME'] = "consultjsmith@nypl.org"
-# ENV['CIRC_PASSWORD'] = "turtlepower"
-
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
