98eca31087a75e3709972d76fcf4a673ecb5f6cd
jcsmit17 <jonathansmith7656@gmail.com>
2021-06-20 19:44:05 -0400

    debug present?

--- pages/circ_admin_page.rb
+++ pages/circ_admin_page.rb
@@ -159,7 +159,7 @@ class CircAdminLibrariesForm < CircAdminPage
 
     # PAGE ACTIONS
     def fill_form (name, url, patron_support_email, vendor_email)
-        @library_create_button.present?
+        puts @library_create_button.present?
         @library_create_button.wait_until(&:present?).click
         # wait_for_loading_message(@library_loading_message)
         sleep(30)
